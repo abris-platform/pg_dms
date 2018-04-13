e93046f46785dd7cdfea3774295b16f9ac74defa
Aleksey Novikov <novikov82@gmail.com>
2018-04-13 11:16:49 +0300

    db clean script

---
+++ sql/pgdms_clean.sql
@@ -0,0 +1,8 @@
+drop SEQUENCE public.dup_key_seq cascade;
+drop SEQUENCE public.dn_key_seq cascade;
+drop TABLE public.dup cascade;
+drop TABLE public.dn;
+drop TABLE public.d cascade;
+drop EXTENSION pgdms;
+
+
